df86f6e6c2c742b4e5cbb2297af92ea9c5db359b
Ryuichi <2540987771@qq.com>
2025-04-16 18:16:39 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ryuichi on 2025/4/16.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

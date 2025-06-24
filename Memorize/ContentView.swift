031af194bac7c09816d57926e972c419b36e7796
Ryuichi <2540987771@qq.com>
2025-06-24 11:42:51 +0800

    delete CardView()

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         return HStack(content: {
             CardView(isFaceUp: true)
             CardView()
-            CardView()
+            
         })
     }
 }
